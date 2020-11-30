46ab2b21da70016e1f600e9abbf1d823341e15e2
ashokbotcha <43179022+ashokbotcha@users.noreply.github.com>
2020-11-30 23:36:45 +0800

    ..

---
+++ pet-clinic-data/start.groovy
@@ -0,0 +1,5 @@
+@Grab("com.example:spring-boot-starter-actuator:0.5.0.BUILD-SNAPSHOT")
+@Grab("com.example:spring-boot-starter-web:0.5.0.BUILD-SNAPSHOT")
+@ComponentScan("com.example.sample.service")
+class Start {
+}
